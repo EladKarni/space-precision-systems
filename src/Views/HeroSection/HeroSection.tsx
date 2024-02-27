37f3bf5dcff77d080e9759eee25b2516028ab3e1
Elad Karni <elad.karni@gmail.com>
2024-02-26 23:29:43 -0500

    Fix: Updated Hero

--- src/Views/HeroSection/HeroSection.tsx
+++ src/Views/HeroSection/HeroSection.tsx
@@ -1,19 +1,23 @@
 const HeroSection = () => {
   return (
-    <div className="hero w-screen">
-      <video loop autoPlay muted>
+    <div className="hero w-screen bg-black h-[50vh] md:h-[75vh]">
+      <video loop autoPlay muted className="h-[50vh] md:h-[75vh]">
         <source src="bg-earth-rotating.webm" type="video/mp4" />
       </video>
       <div className="hero-overlay bg-opacity-60"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-          <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="mb-5">
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
+            a id nisi.
+          </p>
           <button className="btn btn-primary">Get Started</button>
         </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default HeroSection;
