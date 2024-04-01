0fc9d129c9a7cf24ecd4faafcf9f72c99dcaf6b1
Elad Karni <elad.karni@gmail.com>
2024-04-01 17:58:14 -0400

    Fix: Updated naming for form elements

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -13,10 +13,22 @@ const contact = () => {
         method="post"
         data-netlify="true"
       >
-        <label className="input input-bordered flex items-center gap-2">
-          <input type="text" className="grow" placeholder="Name" />
+        <label
+          className="input input-bordered flex items-center gap-2"
+          htmlFor="yourname"
+        >
+          <input
+            type="text"
+            className="grow"
+            placeholder="Name"
+            name="name"
+            id="yourname"
+          />
         </label>
-        <label className="input input-bordered flex items-center gap-2">
+        <label
+          className="input input-bordered flex items-center gap-2"
+          htmlFor="yourorg"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
@@ -25,9 +37,18 @@ const contact = () => {
           >
             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
           </svg>
-          <input type="text" className="grow" placeholder="Organization" />
+          <input
+            type="text"
+            className="grow"
+            placeholder="Organization"
+            name="org"
+            id="yourorg"
+          />
         </label>
-        <label className="input input-bordered flex items-center gap-2">
+        <label
+          className="input input-bordered flex items-center gap-2"
+          htmlFor="youremail"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
@@ -37,9 +58,18 @@ const contact = () => {
             <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
             <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
           </svg>
-          <input type="text" className="grow" placeholder="Email" />
+          <input
+            type="text"
+            className="grow"
+            placeholder="Email"
+            name="email"
+            id="youremail"
+          />
         </label>
-        <label className="input input-bordered flex items-center gap-2">
+        <label
+          className="input input-bordered flex items-center gap-2"
+          htmlFor="yourphone"
+        >
           <svg
             aria-hidden="true"
             focusable="false"
@@ -52,11 +82,19 @@ const contact = () => {
           >
             <path d="M493.4 24.6l-104-24c-11.3-2.6-22.9 3.3-27.5 13.9l-48 112c-4.2 9.8-1.4 21.3 6.9 28l60.6 49.6c-36 76.7-98.9 140.5-177.2 177.2l-49.6-60.6c-6.8-8.3-18.2-11.1-28-6.9l-112 48C3.9 366.5-2 378.1.6 389.4l24 104C27.1 504.2 36.7 512 48 512c256.1 0 464-207.5 464-464 0-11.2-7.7-20.9-18.6-23.4z" />
           </svg>
-          <input type="text" className="grow" placeholder="Phone" />
+          <input
+            type="text"
+            className="grow"
+            placeholder="Phone"
+            name="phone"
+            id="yourphone"
+          />
         </label>
         <textarea
           className="input input-bordered flex items-center gap-2 min-h-48"
           placeholder="Message"
+          name="message"
+          id="yourmessage"
         />
         <button type="submit" className="btn btn-primary w-40 mb-4">
           Send
